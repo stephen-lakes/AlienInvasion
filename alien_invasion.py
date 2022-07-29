7d7c941c5e27b5dee2c7ac4e6e7b88d8ab3b8974
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-29 20:30:46 +0100

    Update: import button from Button, created button instance play_button, this button instance wil be used to start the game, passed play_button to update_screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Group
 
+from button import Button
 import game_functions as gf
 from settings import Settings
 from ship import Ship
@@ -13,6 +14,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make the play button.
+    play_button = Button(ai_settings, screen, "Play")
 
     # Create an instance to store game statistics.
     stats = GameStats(ai_settings)
@@ -33,7 +37,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, ship, aliens)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
