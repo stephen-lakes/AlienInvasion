8253a13f6adf8069d8d55127187b2984b6de0a1a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-30 13:15:18 +0100

    Update: draw the level_image to the screen

--- scoreboard.py
+++ scoreboard.py
@@ -59,3 +59,4 @@ class ScoreBoard():
         """ Draw score to the screen. """
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
