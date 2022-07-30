491967dfcd89359a44aeee358d5bf17ce81da5fb
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-30 13:36:38 +0100

    Update: reset scoreboard, updated check_event definiton and passed sb to check_event call

--- game_functions.py
+++ game_functions.py
@@ -27,9 +27,11 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
                 check_keyup_events(event, ship)
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = pygame.mouse.get_pos()
-                check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
+                check_play_button(ai_settings, screen, stats, sb, play_button,
+                                    ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, sb, play_button,
+    ship, aliens, bullets, mouse_x, mouse_y):
     """ Start a new game when the player click Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
@@ -38,9 +40,15 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
 
         # Hide the mouse cursor.
         pygame.mouse.set_visible(False)
+        
         # Reset the game statistics.
         stats.reset_stats()
         stats.game_active = True
+
+        # Reset the scoreboard images.
+        sb.prep_score()
+        sb.prep_high_score()
+        sb.prep_level()
 
         # Empty the list of aliens and bullets.
         aliens.empty()
