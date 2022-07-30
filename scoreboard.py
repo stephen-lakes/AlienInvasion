7b9a205260a6f3d6915146a61c3a27f9004568b6
stephen-lakes <oluyomiolamilekan99@gmail.com>
2022-07-30 13:58:02 +0100

    Update: updated show_score to draw ships left

--- scoreboard.py
+++ scoreboard.py
@@ -72,3 +72,5 @@ class ScoreBoard():
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        # Draw ships.
+        self.ships.draw(self.screen)
